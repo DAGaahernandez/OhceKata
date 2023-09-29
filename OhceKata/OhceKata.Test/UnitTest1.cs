abed9644f9d8719c522cebcb635792b16e563075
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-09-29 12:40:42 +0100

    feat: give_greetings_to_user

--- OhceKata/OhceKata.Test/UnitTest1.cs
+++ OhceKata/OhceKata.Test/UnitTest1.cs
@@ -1,11 +1,46 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace OhceKata.Test;
 
-public class Tests {
+public class OhceShould {
     [SetUp]
-    public void Setup() { }
+    public void Setup() {
+        
+    }
 
     [Test]
-    public void Test1() {
-        Assert.Pass();
+    public void give_greetings_to_user() {
+        //Given;
+        
+        //When
+        var console = new MockConsole();
+        new Ohce(console).Start();
+        
+        //Then
+        Assert.AreEqual(console.LastText, "¡Buenos días Pedro!");
+    }
+}
+
+public interface IConsole {
+    void Write(string message);
+}
+
+public class MockConsole : IConsole {
+    public string LastText { get; private set; }
+
+    public void Write(string message) {
+        LastText = message;
+    }
+}
+
+public class Ohce {
+    private readonly IConsole _console;
+
+    public Ohce(IConsole console) {
+        _console = console;
+    }
+
+    public void Start() {
+        _console.Write("¡Buenos días Pedro!");
     }
 }
--- OhceKata/OhceKata/Program.cs
+++ OhceKata/OhceKata/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+args = args.Reverse().ToArray();
 Console.WriteLine("Hello, World!");
